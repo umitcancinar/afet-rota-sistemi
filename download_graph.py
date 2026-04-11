b1ff8306b269e005dd53c5e7d454e94da335f088
Selim Yağbasan <250541111@firat.edu.tr>
2026-04-11 10:39:01 +0300

    Remove download_and_save_graph function
    Removed the download_and_save_graph function from the script.

--- download_graph.py
+++ download_graph.py
@@ -15,16 +15,5 @@ def download_and_save_graph(city_name="Antakya, Hatay, Turkey", filename="antaky
     except Exception as e:
         logger.error(f"Error: {e}")
 
-def download_and_save_graph(city_name="Elazığ , Elazığ ,  Turkey", filename="elazıg_graph.graphml"):
-    logger.info(f"Downloading graph for {city_name}...")
-    try:
-        G = ox.graph_from_place(city_name, network_type="drive")
-        logger.info(f"Saving graph to {filename}...")
-        ox.save_graphml(G, filepath=filename)
-        logger.info("Done!")
-    except Exception as e:
-        logger.error(f"Error: {e}")
-
-
 if __name__ == "__main__":
     download_and_save_graph()
